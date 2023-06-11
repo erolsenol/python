ceb5d1bd98e65b63fb92f175cd93fb47c22704cf
erolsenol <erol.senol@outlook.com>
2023-06-11 23:56:52 +0300

    emulator open and screen take ss completed

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
 
+import pygetwindow
+import pyautogui
 import subprocess
 import psutil
+
+from window_ss import ImageProccess
 
 emulatorSrc = r"C:\Program Files\BlueStacks_nxt\HD-Player.exe"
 emulatorName = "HD-Player.exe"
 comMyDefence = "com.my.defense"
 
-adb_komutu = 'adb shell am start -n com.my.defense/com.unity3d.player.UnityPlayerActivity'
+adb_command = 'adb shell am start -n com.my.defense/com.unity3d.player.UnityPlayerActivity'
+
 
 def emulatorHasRuning():
     # Tüm çalışan süreçleri al
@@ -19,19 +24,19 @@ def emulatorHasRuning():
 
     runing = False
     for surec in allProcess:
-        if emulatorName in surec.name(): runing = True
+        if emulatorName in surec.name():
+            runing = True
     return runing
- 
+
 
 def start():
     if not emulatorHasRuning():
         openEmulator()
     else:
         # subprocess.Popen([emulatorSrc, f"--package=com.my.defense"])
-        subprocess.Popen(adb_komutu, shell=True)
+        subprocess.Popen(adb_command, shell=True)
         print("emulator runing")
     return
-
 
 
 def openEmulator():
@@ -39,6 +44,13 @@ def openEmulator():
         subprocess.Popen(emulatorSrc)
     except:
         print("Emulator default src not runing")
+
+def test():
+    ImageProccess.takeSS()
+    # window = pygetwindow.getWindowsWithTitle("BlueStacks")[0]
+    # print(window)
+    return
+
 
 class MyPaintApp(App):
 
@@ -50,11 +62,9 @@ class MyPaintApp(App):
         return parent
 
     def clear_canvas(self, obj):
-        start()
+        test()
+        # start()
 
 
 if __name__ == '__main__':
     MyPaintApp().run()
-
-
-
